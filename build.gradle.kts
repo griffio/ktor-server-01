b1f4f239ff52c2c98c20457dd22275c3f23a6560
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-01-05 08:57:30 +0000

    Update gradle build
    latest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.3"
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.7"
 }
 
 val ktorVersion: String by project
